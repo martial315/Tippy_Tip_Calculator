b149cb889b6ceb60eb6ad86439aeba8bc47f2080
Jesse Spangler <budboy315@gmail.com>
2024-01-26 15:59:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.tippy
+
+import android.animation.ArgbEvaluator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
+private const val INITIAL_SPLIT = 1
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount : EditText
+    private lateinit var tvTipPercent : TextView
+    private lateinit var seekBarTip : SeekBar
+    private lateinit var tvTipAmount : TextView
+    private lateinit var tvTotalAmount : TextView
+    private lateinit var tvTipDescription : TextView
+    private lateinit var etSplitNum : EditText
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        tvTipPercent = findViewById(R.id.tvTipPercent)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+        etSplitNum = findViewById(R.id.etSplitNum)
+        updateTipDescription(INITIAL_TIP_PERCENT)
+
+        etSplitNum.setText("$INITIAL_SPLIT")
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercent.text = "$INITIAL_TIP_PERCENT%"
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress")
+                tvTipPercent.text = "$progress%"
+                computeTipAndTotal()
+                updateTipDescription(progress)
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+        etBaseAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterTextChanged $s")
+                computeTipAndTotal()
+            }
+        })
+
+        etSplitNum.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterTextChanged $s")
+                computeTipAndTotal()
+            }
+
+        })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when(tipPercent){
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"
+        }
+        tvTipDescription.text = tipDescription
+        //update the color based on tipPercent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.color_worst_tip),
+            ContextCompat.getColor(this, R.color.color_best_tip)
+        ) as Int
+        tvTipDescription.setTextColor(color)
+    }
+
+    private fun computeTipAndTotal() {
+        if(etBaseAmount.text.isEmpty()){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        if(etSplitNum.text.isEmpty()){
+            return
+        }
+        //1. Get values of base and tip percent
+        val baseAmount = etBaseAmount.text.toString().toDouble()
+        val numSplit = etSplitNum.text.toString().toDouble()
+        var tipPercent = seekBarTip.progress
+        //2. compute the tip and total amount due
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount = (tipAmount + baseAmount) / numSplit
+        //3. update the ui
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text = "%.2f".format(totalAmount)
+
+
+    }
+}
+
+
